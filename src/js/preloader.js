f6511cf3a1fe3a78f68e0bcede066ecdafa6d2a6
bw <i@baldworm.ru>
2016-08-25 07:03:23 +0300

    added animation text

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,18 +1,52 @@
 /**
  * Created by bw on 25.08.2016.
  */
-function showPreloader (sec) {
-    var body = document.body;
+var preloader = new Preloader();
 
-        //adding background
-    var backDiv = document.createElement("div");
-    backDiv.id = "preloader_background";
-    body.appendChild(backDiv);
+function Preloader(){
+    var PRELOADER_BACK_DIV_ID = "preloader_background";
+    var LOADING_TEXT = "Loading";
+    var ANIMATION_TEXT = " . . . ";
 
-    setTimeout(hidePreloader,3000);
+    var span = document.createElement("span");
+    var backDiv = createBackDiv();
+    backDiv.appendChild(span);
+
+    var frame = 0;
+
+    this.show = function (){
+        document.body.appendChild(backDiv);
+        this.timerId = setInterval(animate,300);
+
+    }
+    this.hide = function (){
+        document.body.removeChild(backDiv);
+        clearInterval(this.timerId)
+    }
+
+    function animate(){
+        setTextToSpan();
+        frame++;
+        if (frame > ANIMATION_TEXT.length)
+            frame = 0;
+
+    }
+    function createBackDiv (){
+        var div = document.createElement("div");
+        div.id = PRELOADER_BACK_DIV_ID;
+        div.textContent = LOADING_TEXT;
+        return div;
+    }
+
+    function setTextToSpan (){
+        span.textContent = ANIMATION_TEXT.slice(0, frame);
+    }
+}
+function showPreloader () {
+    preloader.show();
+    setTimeout(hidePreloader,4000);
 
 }
 function hidePreloader(){
-    var backDiv = document.getElementById("preloader_background");
-    backDiv.parentNode.removeChild(backDiv);
+    preloader.hide();
 }
